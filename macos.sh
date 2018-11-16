d22c11f09355ca14edc05791a4eb7d34ec60f81e
Ben Burwell <ben@benburwell.com>
2018-11-16 15:58:50 -0500

    Split roles

--- macos.sh
+++ macos.sh
@@ -2,6 +2,6 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install ansible
-ansible-pull -U https://github.com/benburwell/bootstrap.git --directory /tmp/ansible-bootstrap --purge macos-workstation-local.yml
+ansible-pull -U https://github.com/benburwell/bootstrap.git --directory /tmp/ansible-bootstrap --purge main.yml
 
 #brew services start offlineimap
