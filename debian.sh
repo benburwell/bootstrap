64d9014a4ecf70350013e6ad861ccf7a9daffdd7
Ben Burwell <ben@benburwell.com>
2018-11-20 10:03:26 -0500

    Use latest ansible

--- debian.sh
+++ debian.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
+#
+# To run:
+#
+#   $ wget --no-check-certificate https://raw.githubusercontent.com/benburwell/bootstrap/master/debian.sh
+#   $ chmod +x debian.sh
+#   $ ./debian.sh
+
+echo "Installing sudo, git, dirmngr"
+apt update
+apt install -y sudo git dirmngr
 
 echo "Adding Ansible apt repo..."
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
-echo "Installing sudo, ansible, git..."
+echo "Installing ansible..."
 apt update
-apt install sudo ansible git
+apt install -y ansible
 
 echo "Adding bb to sudo group..."
 usermod bb --append --groups sudo
